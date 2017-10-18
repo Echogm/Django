13d44f10df033add96792f12f5ff6b8e81891a8a
eduardo <silverk2112@gmail.com>
2017-10-18 16:35:53 -0400

    Added new functionality

--- projects/main/apps/blogs/urls.py
+++ projects/main/apps/blogs/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url('^$', views.index),
-    url('^new$', views.new)
-    url('^create$',views.create)
+    url('^new$', views.new),
+    url('^create$', views.create),
+    url('^(?P<number>\d+)$', views.show),
+    url('^(?P<number>\d+)/edit$', views.edit),
+    url('^(?P<number>\d+)/delete$', views.delete)
 ]
--- projects/main/apps/blogs/views.py
+++ projects/main/apps/blogs/views.py
@@ -3,11 +3,27 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
+# Indexpage.
 def index(request):
-    response = "Index"
+    response = "Blog"
     return HttpResponse(response)
+
 def new(request):
     response = "New form for a blogs post."
     return HttpResponse(response)
+
 def create(request):
     response = "Create a new post here."
+    return HttpResponse(response)
+
+def show(request, number):
+    response = "Place Holder id {}".format(number)
+    return HttpResponse(response)
+
+def edit(request,number):
+    response = "Edit Post {}".format(number)
+    return HttpResponse(response)
+
+def delete(request,number):
+    response = "Delete Post {}".format(number)
+    return HttpResponse(response)
