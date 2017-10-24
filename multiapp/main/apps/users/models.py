e7adc85f0fd4be77e9cfc717e5da02d03d4345cc
eduardo <silverk2112@gmail.com>
2017-10-24 17:30:08 -0400

    First Commit

--- multiapp/main/apps/users/models.py
+++ multiapp/main/apps/users/models.py
@@ -1,6 +1,64 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
-
+from django.core.validators import validate_email
+import bcrypt
 # Create your models here.
+# try:
+#     validate_email(email.POST["email"])
+#     valid_email = True
+# except validate_email,ValidationError:
+#     valid_email = False
+class UserManager(models.Manager):
+    def register(self, name, last, email, password, confirmation):
+
+        errors = []
+
+        if len(name) < 3:
+            errors.append("Name most be 2 characters or longer!")
+        if len(last) < 3:
+            errors.append("Last name must be 2 characters or longer!")
+        if len(Users.userManager.filter(email=email)) > 0:
+            errors.append("This email is already taken.")
+        if len(password) < 8:
+            errors.append("Password most be 8 characters or longer!")
+        if not password == confirmation:
+            errors.append("Passwords must match.")
+        if len(errors) > 0:
+            return(False, errors)
+        else:
+            passwordHashed =bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            user = Users.userManager.create(name=name, lastName=last, email=email, passwordHashed=passwordHashed)
+            return (True, user)
+    def login(self, email, password):
+        errors = []
+
+        if len(email) < 2:
+            errors.append("Email must be 2 characters or longer!")
+        if len(password) < 8:
+            errors.append("Password must be 8 characters or longer!")
+
+        email = Users.userManager.filter(email=email)
+
+        if len(email) == 0:
+            errors.append("Email not found!")
+
+        if len(errors) > 0:
+            return (False, errors)
+
+        else:
+            if bcrypt.checkpw(password.encode(), email[0].passwordHashed.encode()):
+                return (True, email[0])
+            else:
+                return (False, ["Incorrect Password!"])
+
+
+class Users(models.Model):
+    name = models.CharField(max_length = 255)
+    lastName = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    passwordHashed = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    userManager = UserManager()
--- multiapp/main/apps/users/views.py
+++ multiapp/main/apps/users/views.py
@@ -1,12 +1,62 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login
+from .models import Users
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, "users/usersIndex.html")
 def register(request):
-    return render(request, "users/usersRegister.html")
-def login(request):
-    return render(request, "users/usersLogin.html")
+    if request.method == 'POST':
+        valid = Users.userManager.register(
+            request.POST['name'],
+            request.POST['last'],
+            request.POST['email'],
+            request.POST['password'],
+            request.POST['confirmation']
+        )
+        if valid[0]:
+		    request.session["email"] = {
+		    "id": valid[1].id,
+		    "name": valid[1].name
+		    }
+                    return redirect("users/users")
+        else:
+            for errors in valid[1]:
+                messages.add_message(request, messages.ERROR, errors)
+            return redirect("/register")
+    elif request.method == 'GET':
+        return render(request, "users/usersRegister.html")
+def new_session(request):
+    if request.method == 'POST':
+        valid = Users.userManager.login(
+            request.POST["email"],
+            request.POST["password"],
+        )
+        if valid[0]:
+            request.session["email"] = {
+		        "id": valid[1].id,
+		        "name": valid[1].name
+		    }
+            return redirect("/users")
+        else:
+            for errors in valid[1]:
+                messages.add_message(request, messages.ERROR, errors)
+        return redirect("/login")
+    if request.method == 'GET':
+        return render(request, "users/usersLogin.html")
+    
+    # email = request.POST.get['email']
+    # password = request.POST.get['password']
+    # user = authenticate(email=email, password=password)
+    # if user is not False:
+    #     if user is True:
+    #         # login(request, user)
+    #         return redirect("/users")
+    #     else:
+    #         return redirect("/login")
+    # else:
+    #     return redirect("/login")
 def users(request):
     return render(request, "users/usersList.html")
